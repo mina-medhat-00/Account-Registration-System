0efb6e6e26440b760d87caa57e87db81ee49005d
Mina Medhat <71275503+mina-medhat-00@users.noreply.github.com>
2023-08-08 15:47:20 +0300

    Create routes

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+//Routes
+app.use("/", require("./routes/index"));
+
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+// Login page
+router.get("/", (req, res) => res.send("Welcome"));
+
+module.exports = router;
