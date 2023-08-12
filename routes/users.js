87edfa924f7b7cad8ce541b30e3851d258aa690d
Mina Medhat <71275503+mina-medhat-00@users.noreply.github.com>
2023-08-12 23:18:31 +0300

    Update layout

--- routes/users.js
+++ routes/users.js
@@ -8,8 +8,34 @@ router.get("/login", (req, res) => res.render("login"));
 router.get("/register", (req, res) => res.render("register"));
 
 // Register Handle
-router.post("./register", (req, res) => {
+router.post("/register", (req, res) => {
   const { name, email, password, password2 } = req.body;
+  let errors = [];
+  // Check required fields
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: "Please fill in all fields" });
+  }
+  // Check passwords match
+  if (password !== password2) {
+    errors.push({ msg: "Passwords do not match" });
+  }
+  // Check pass length
+  if (password.length < 6) {
+    errors.push({ msg: "Passwords should be at least 6 characters" });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", {
+      errors,
+      name,
+      email,
+      password,
+      password2,
+    });
+  } else {
+    // 
+    res.send("pass");
+  }
 });
 
 module.exports = router;
