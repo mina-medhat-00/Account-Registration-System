78c972924655cced2c03ae057e470376f19ccca3
Mina Medhat <71275503+mina-medhat-00@users.noreply.github.com>
2023-08-14 19:23:34 +0300

    Modify keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   MongoURI:
-    "mongodb+srv://Mina:31rUL8hfD2inZYjT@cluster0.fr4rtjl.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://<username>:<password>@cluster0.fr4rtjl.mongodb.net/?retryWrites=true&w=majority",
 };
